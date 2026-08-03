397e83475e6e210a265d7ad30d271e7869db1b22
SHERIN S <sherinsher1906@gmail.com>
2026-08-03 03:09:12 -0700

    Created Flask templates and home page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Welcome to CivicConnect - Public Infrastructure Issue Reporting App"
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
