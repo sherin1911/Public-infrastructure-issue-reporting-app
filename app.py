57936c5a26ea6d6dc8f8f2017644a9d0e8275fbd
SHERIN S <sherinsher1906@gmail.com>
2026-08-05 09:09:27 -0700

    Implemented user registration with SQLite database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +7,31 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("civicconnect.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            (name, email, password),
+        )
+
+        conn.commit()
+        conn.close()
+
+        return "Registration Successful!"
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
